605e23846cc05dd8e6701611a38b8897c1d52811
Adam Dadswell <AdamDadswell@rentalcars.com>
2019-06-05 08:22:59 +0100

    Changed Test Name

--- src/__test__/index.spec.js
+++ src/__test__/index.spec.js
@@ -1,7 +1,7 @@
 const romanNumerals = require('../index').romanNumerals;
 
 describe('Roman Numerals', () => {
-    test('1 to I', () => {
+    test('testing 1 to I', () => {
         expect(romanNumerals(1)).toBe('I');
     });
 });
