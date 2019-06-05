1280beb1a29f8cd9cbb4f361ae78f54cf110fea5
Adam Dadswell <AdamDadswell@rentalcars.com>
2019-06-05 10:12:36 +0100

    Formatting

--- src/__test__/index.spec.js
+++ src/__test__/index.spec.js
@@ -1,12 +1,15 @@
 const romanNumerals = require('../index').romanNumerals;
 
 describe('Roman Numerals', () => {
+   
     test('testing 1 to I', () => {
         expect(romanNumerals(1)).toBe('I');
     });
+   
     test('testing 2 to II', () => {
         expect(romanNumerals(2)).toBe('II');
     });
+   
     test('testing 3 to III', () => {
         expect(romanNumerals(3)).toBe('III');
     });
