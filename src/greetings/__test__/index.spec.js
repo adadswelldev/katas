947f5cb4af9de7068db3e8ad51cb9eb0b51934ed
Adam Dadswell <AdamDadswell@rentalcars.com>
2019-06-06 14:33:09 +0100

    Updated Greeting Kata

--- src/greetings/__test__/index.spec.js
+++ src/greetings/__test__/index.spec.js
@@ -4,16 +4,4 @@ describe('Greetings', () => {
     test('Simple Greeting', () => {
         expect(greeting('Bob')).toBe('Hello Bob!');
     });
-
-    test('General Greeting', () => {
-        expect(greeting()).toBe('Hello Stranger!');
-    });
-
-    test('Greeting None String', () => {
-        expect(greeting(23)).toBe('Hello Stranger!');
-    });
-    
-    test('Shouting Greeting', () => {
-        expect(greeting('BOB')).toBe('HELLO BOB!');
-    });
 });
--- src/greetings/index.js
+++ src/greetings/index.js
@@ -1,12 +1,3 @@
 module.exports.greeting = (name) => {
-
-    if (typeof name == 'string') {
-            if (name === name.toUpperCase()) {
-                    return 'HELLO ' + name + '!';
-            } else {
-                    return 'Hello ' + name + '!';
-            }
-    } else {
-            return 'Hello Stranger!';                
-    }
-};
+    return 'Hello Bob!';
+}
